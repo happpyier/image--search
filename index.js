f4a5012e89fb50384611ccba02900139c5faeb17
Thomas Bernfeld <happpyier@gmail.com>
2016-03-16 11:23:19 -0400

    From Work Setting up initial files

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/latest', function(request, response) {
   response.send('This is the latest query page.');
   //response.end('Its Over!'); 
 });
-app.get('//:id', function(request, response) {
+app.get('/:id', function(request, response) {
   response.send('This is the search page.<br/>Your query is <br/>'+request.params.id);
   //response.end('Its Over!'); 
 });
