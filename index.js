09556e8e8392ba7092360e47b6f1bd44883bf065
Thomas Bernfeld <happpyier@gmail.com>
2016-03-16 11:06:56 -0400

    From Work Inital Install

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname+'/index.html'));
   //response.end('Its Over!'); 
 });
+app.get('/latest', function(request, response) {
+  response.send('This is the latest query page.');
+  //response.end('Its Over!'); 
+});
+app.get('//:id', function(request, response) {
+  response.send('This is the search page.<br/>Your query is <br/>'+request.params.id);
+  //response.end('Its Over!'); 
+});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
