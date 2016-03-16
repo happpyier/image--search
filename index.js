89a624af187fe010acc830ed09a65cd2b57c7af4
Thomas Bernfeld <happpyier@gmail.com>
2016-03-16 11:27:37 -0400

    From Work Setting up initial files

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/latest', function(request, response) {
 });
 app.get('/:id', function(request, response) {
   var parameters = JSON.stringify(request.params);
-  response.send('This is the search page.<br/>Your query is <br/>'+parameters);
+  response.send('This is the search page.<br/>Your query is <br/>'+request.query);
   //response.end('Its Over!'); 
 });
 app.listen(app.get('port'), function() {
