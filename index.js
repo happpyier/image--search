eccee6b84a0ba524e196b4b628346f918fbaa670
Thomas Bernfeld <happpyier@gmail.com>
2016-03-16 16:16:56 -0400

    From Work Still setting up...Sorta

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*
-http://expressjs.com/en/guide/routing.html USING this as a reference;
-http://stackoverflow.com/questions/20089582/how-to-get-url-parameter-in-express-node-js Also a reference;
+https://developers.google.com/custom-search/docs/tutorial/introduction USING this as a reference;
+http://google.github.io/google-api-nodejs-client/3.1.0/index.html Also a reference;
+https://github.com/google/google-api-nodejs-client/ Also a reference;
 */
 var google = require('googleapis');
 var express = require('express');
@@ -11,17 +12,15 @@ app.set('port', (process.env.PORT || 5000));
 app.set("Content-Type", "text/html");
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname+'/index.html'));
-  //response.end('Its Over!'); 
 });
 app.get('/latest', function(request, response) {
   response.send('This is the latest query page.');
-  //response.end('Its Over!'); 
 });
 app.get('/:id', function(request, response) {
   var parameters1 = JSON.stringify(request.params);
   var parameters2 = JSON.stringify(request.query);
+  response.sendFile(path.join(__dirname+'/searchresults.html'));
   response.send('This is the search page.<br/>Your query is <br/>'+parameters1+' '+parameters2);
-  //response.end('Its Over!'); 
 });
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port')); 
