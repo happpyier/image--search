c031c393c525712d52348582125425a7dcb47dd7
Thomas Bernfeld <happpyier@gmail.com>
2016-03-16 13:50:20 -0400

    From Work Setting up initial files

--- index.js
+++ index.js
@@ -24,13 +24,14 @@ app.get('/:id', function(request, response) {
   var parameters1 = JSON.stringify(request.params);
   var parameters2 = JSON.stringify(request.query);
   urlshortener.url.get(params, function (err, response) {
-  if (err) {
-    console.log('Encountered error', err);
-  } else {
-    console.log('Long url is', response.longUrl);
-  }
+    if (err) {
+      console.log('Encountered error', err);
+    } else {
+      console.log('Long url is', response.longUrl);
+    }
+  
+  });
   response.send('This is the search page.<br/>Your query is <br/>'+parameters1+' '+parameters2);
-});
   //response.end('Its Over!'); 
 });
 app.listen(app.get('port'), function() {
