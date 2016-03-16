e39d24a41bb4bc8ac30165befbb2bdd59382296f
Thomas Bernfeld <happpyier@gmail.com>
2016-03-16 13:43:27 -0400

    From Work Setting up initial files

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 http://expressjs.com/en/guide/routing.html USING this as a reference;
 http://stackoverflow.com/questions/20089582/how-to-get-url-parameter-in-express-node-js Also a reference;
 */
+var google = require('googleapis');
+var urlshortener = google.urlshortener('v1');
+var params = { shortUrl: 'http://goo.gl/xKbRu3' };
+// get the long url of a shortened url
 var express = require('express');
 var app = express();
 var path = require("path");
@@ -20,6 +24,13 @@ app.get('/:id', function(request, response) {
   var parameters1 = JSON.stringify(request.params);
   var parameters2 = JSON.stringify(request.query);
   response.send('This is the search page.<br/>Your query is <br/>'+parameters1+' '+parameters2);
+  urlshortener.url.get(params, function (err, response) {
+  if (err) {
+    console.log('Encountered error', err);
+  } else {
+    console.log('Long url is', response.longUrl);
+  }
+});
   //response.end('Its Over!'); 
 });
 app.listen(app.get('port'), function() {
