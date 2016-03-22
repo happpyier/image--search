8e2f59c3e0ce2b146e00d897b3c2dd6dddb9ba27
Thomas Bernfeld <happpyier@gmail.com>
2016-03-22 14:26:52 -0400

    From Work doing stuffs

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/:id', function(request, response) {
 	*/
 	//response.sendFile(path.join(__dirname+'/searchresults.html'));
 	urlsearch({ auth: API_KEY, cx:'012239477241375126935:swwmv-c4dsi', q:'Ryuu+Lavitz' }, function(err, user) {
-	  response.send((err ? err.message : 'It worked'));
+	  response.send((err ? 'it Failed' : 'It worked'));
 	});
 	//response.end('all done');
 	
