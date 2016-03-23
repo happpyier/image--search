1ccc9b2fb85ccf62aea06493d00ac2baf0119c28
Thomas Bernfeld <happpyier@gmail.com>
2016-03-23 09:48:20 -0400

    From Work Setting up the searchresults page

--- index.js
+++ index.js
@@ -38,30 +38,21 @@ app.get('/:id', function(request, response) {
 	});
 	*/
 	//response.sendFile(path.join(__dirname+'/searchresults.html'));
-	//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-	var options = 
-	{
-	  host: 'www.random.org',
-	  path: '/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-	};
-
-	callback = function(response) 
-	{
-	  var str = '';
-
-	  //another chunk of data has been recieved, so append it to `str`
-	  response.on('data', function (chunk) {
-		str += chunk;
+	http.createServer(function(request, response) {
+	  var headers = request.headers;
+	  var method = request.method;
+	  var url = request.url;
+	  var body = [];
+	  request.on('error', function(err) {
+		console.error(err);
+	  }).on('data', function(chunk) {
+		body.push(chunk);
+	  }).on('end', function() {
+		body = Buffer.concat(body).toString();
+		// At this point, we have the headers, method, url and body, and can now
+		// do whatever we need to in order to respond to this request.
 	  });
-
-	  //the whole response has been recieved, so we just print it out here
-	  response.on('end', function () {
-		console.log(str);
-	  });
-	  response.end('All Done');
-	}
-	var req = http.request(options, callback);
-	req.end();
+	}).listen(8080); // Activates this server, listening on port 8080.
 	
 });
 
