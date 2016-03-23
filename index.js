17d9fce1d920ae1529ef5081f4c3793a2eeb257c
Thomas Bernfeld <happpyier@gmail.com>
2016-03-23 12:08:32 -0400

    From Work Find the address

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/:id', function(request, response) {
 	//response.sendFile(path.join(__dirname+'/searchresults.html'));
 	
 
-	urlsearch.cse.list({ cx: cxId, key: secretKey, q: 'RyuuLavitz' }, function(err, user) {
+	urlsearch.cse.list({ q: 'RyuuLavitz' }, function(err, user) {
 	  response.send(err ? err : user);
 	});
 	//response.end('all done');
