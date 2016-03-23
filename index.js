a7d59abb8692f74de5023b161765024904f17001
Thomas Bernfeld <happpyier@gmail.com>
2016-03-23 09:20:47 -0400

    From Work Setting up the searchresults page

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.get('/:id', function(request, response) {
 	});
 	*/
 	//response.sendFile(path.join(__dirname+'/searchresults.html'));
-	urlsearch.get({ auth: API_KEY, cx:'012239477241375126935:swwmv-c4dsi' }, function(err, user) {
-	  response.send((err ? 'it Failed' : 'It worked'));
+	urlsearch.get({ auth: API_KEY, cx: cxId }, function(err, user) {
+	  response.send(err ? 'it Failed' : 'It worked');
 	});
 	//response.end('all done');
 	
