bf5d58148a27ca42fec162272ffb7500f73a6b4c
Thomas Bernfeld <happpyier@gmail.com>
2016-03-31 16:28:12 -0400

    From Work Debugging

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.get('/:id', function(request, response) {
   });
   */  
   var API_KEY = secretKey; // specify your API key here
-	urlsearch.cse.list({ cx: cxId,   q: parameters1, num: 10, searchType: 'image', fields: 'items(image/contextLink,link,snippet)', key: 'AIzaSyBO5IZ8i0lpF9I0eMwZ9E4nNV3jXkyUuHM' }, function(err, user) 
+	urlsearch.cse.list({ cx: cxId,   q: parameters1, num: 10, start: parameters2, searchType: 'image', fields: 'items(image/contextLink,link,snippet)', key: API_KEY }, function(err, user) 
 	{
 	  response.send(err ? '<br/>Fail Line<br/>'+err : JSON.stringify(user.items));
 	});	
