a1d05a0d2908771ca4125251a21d3c7e3153c97c
Thomas Bernfeld <happpyier@gmail.com>
2016-04-01 08:31:23 -0400

    From Work Debugging

--- index.js
+++ index.js
@@ -53,10 +53,10 @@ app.get('/:id', function(request, response) {
   var API_KEY = secretKey; // specify your API key here
 	urlsearch.cse.list({ cx: cxId, q: parameters1, num: 10, searchType: 'image', fields: 'items(image/contextLink,link,snippet)', start: parameters2, key: 'AIzaSyBO5IZ8i0lpF9I0eMwZ9E4nNV3jXkyUuHM' }, function(err, user) 
 	{
-	    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('INSERT INTO image_search(term) VALUES ("'+parameters1.id+'")');
-  });
 	  response.send(err ? '<br/>Fail Line<br/>'+err : JSON.stringify(user.items));
+	  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('INSERT INTO image_search(term) VALUES ("'+parameters1.id+'")');
+	});
 	});	
 });
 
