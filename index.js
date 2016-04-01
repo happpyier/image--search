f342e72c35f760d7454dcb7a47500e69b3e7b9c6
Thomas Bernfeld <happpyier@gmail.com>
2016-04-01 15:17:12 -0400

    From Work Debugging DB

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ var pubURL = 'https://cse.google.com:443/cse/publicurl?cx=012239477241375126935:
 var google = require('googleapis');
 var urlsearch = google.customsearch('v1');
 var resultsidSQL = '';
-var failMarker = 'pass';
+var failMarker = 0;
 app.set('port', (process.env.PORT || 5000));
 app.set("Content-Type", "text/html");
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname+'/index.html'));
-  failMarker = 'fail';
+  failMarker = 1;
 });
 app.get('/latest', function(request, response) {
-  failMarker = 'fail';
+  failMarker = 2;
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM image_search ORDER BY search_date DESC LIMIT 10', function(err, result) {
       if (err)
@@ -44,6 +44,7 @@ app.get('/latest', function(request, response) {
 
 });
 app.get('/:id', function(request, response) {
+  failMarker = 3;
   var parameters1 = JSON.stringify(request.params);
   var parametersSQL = parameters1;
   if (parameters2)
@@ -69,7 +70,7 @@ app.get('/:id', function(request, response) {
 	var hour = dateNowVal.getHours();
 	var min = dateNowVal.getMinutes();
 	var dateNowVal = hour+'-'+min+'   '+mm+'-'+dd+'-'+yyyy;
-	if (failMarker == 'pass')
+	if (failMarker == 3)
 	{	
 		pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		client.query("INSERT INTO image_search VALUES ('"+parameters1+"', '"+dateNowVal+"')", function(err, result) {
