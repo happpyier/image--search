9f4a0a2bd86935dece9e1cb217ee69ede64bc8ab
Thomas Bernfeld <happpyier@gmail.com>
2016-04-01 14:17:57 -0400

    From Work Debugging DB

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', function(request, response) {
 });
 app.get('/latest', function(request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM image_search ORDER BY term DESC LIMIT 10', function(err, result) {
+    client.query('SELECT * FROM image_search ORDER BY term DESC LIMIT 1000', function(err, result) {
       if (err)
        //{ resultsSQL = "Error "+ err; response.send("Error " + err);  }
 	   { resultsidSQL = ("Error term" + err); }
