d9d318fc888dd0af566703925aea41cf3e93364c
Thomas Bernfeld <happpyier@gmail.com>
2016-04-01 14:07:39 -0400

    From Work Debugging DB

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.get('/latest', function(request, response) {
   });
   //resultsidSQL = 'Debugging';
   response.send(resultsidSQL);
+  response.end();
 });
 app.get('/:id', function(request, response) {
 	
@@ -50,20 +51,17 @@ app.get('/:id', function(request, response) {
   {
 	parameters2 = 90;
   }
-  if (parametersSQL != 'latest')
-  {
-	  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query("INSERT INTO image_search VALUES ('parametersSQL', 'today')", function(err, result) {
-		  if (err)
-		   //{ resultsSQL = "Error "+ err; response.send("Error " + err);  }
-		   { resultsidSQL = ("Error " + err); }
-		  else
-		   //{ resultsSQL = "Results " + {results: result.rows}; response.render('pages/db', {results: result.rows} ); }
-		   { /*resultsidSQL = JSON.stringify(result.rows[0].id);*/ }
-		   done();
-		});
-	  });
-  }
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+	client.query("INSERT INTO image_search VALUES ('parametersSQL', 'today')", function(err, result) {
+	  if (err)
+	   //{ resultsSQL = "Error "+ err; response.send("Error " + err);  }
+	   { resultsidSQL = ("Error " + err); }
+	  else
+	   //{ resultsSQL = "Results " + {results: result.rows}; response.render('pages/db', {results: result.rows} ); }
+	   { /*resultsidSQL = JSON.stringify(result.rows[0].id);*/ }
+	   done();
+	});
+  });
   var API_KEY = secretKey; // specify your API key here
 	urlsearch.cse.list({ cx: cxId, q: parameters1, num: 10, searchType: 'image', fields: 'items(image/contextLink,link,snippet)', start: parameters2, key: 'AIzaSyBO5IZ8i0lpF9I0eMwZ9E4nNV3jXkyUuHM' }, function(err, user) 
 	{
